106ae3e2b3ba32d717c3f3e06122427981175970
Jack Frost <j4qfrost@gmail.com>
2019-09-10 21:58:49 -0700

    sources

---
+++ backend/pose_estimation.py
@@ -0,0 +1,46 @@
+import cv2
+from gluoncv import model_zoo, data, utils
+from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
+
+detector = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True)
+pose_net = model_zoo.get_model('simple_pose_resnet18_v1b', pretrained=True)
+
+# Note that we can reset the classes of the detector to only include
+# human, so that the NMS process is faster.
+
+detector.reset_class(["person"], reuse_weights=['person'])
+
+def process_pose_frame(frame):
+	x, img = data.transforms.presets.ssd.transform_test(frame, short=512)
+
+	class_IDs, scores, bounding_boxs = detector(x)
+
+	pose_input, upscale_bbox = detector_to_simple_pose(img, class_IDs, scores, bounding_boxs)
+
+	predicted_heatmap = pose_net(pose_input)
+	pred_coords, confidence = heatmap_to_coord(predicted_heatmap, upscale_bbox)
+
+	## (4) Display 
+	for j in range(len(pred_coords)):
+		for i in range(len(pred_coords[0])):
+			#print(class_IDs.reshape(-1))
+			#print(scores.reshape(-1))
+			x, y = pred_coords[j][i].astype(int).asnumpy()
+			cv2.circle(img, (x,y), 2, (0, 255, 0), thickness=-1, lineType=cv2.FILLED)
+			# cv2.putText(img, tag, (x, y-20),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+
+	# for i in range(len(scores[0])):
+	#     #print(class_IDs.reshape(-1))
+	#     #print(scores.reshape(-1))
+	#     cid = int(class_IDs[0][i].asnumpy())
+	#     cname = detector.classes[cid]
+	#     score = float(scores[0][i].asnumpy())
+	#     if score < 0.5:
+	#         break
+	#     x,y,w,h = bbox =  bounding_boxs[0][i].astype(int).asnumpy()
+	#     print(cid, score, bbox)
+	#     tag = "{}; {:.4f}".format(cname, score)
+	#     cv2.rectangle(img, (x,y), (w, h), (0, 255, 0), 2)
+	#     cv2.putText(img, tag, (x, y-20),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+
+	return img
---
+++ backend/pose_estimation_consumer.py
@@ -0,0 +1,49 @@
+import subprocess, sys, time
+
+import json
+
+import pose_estimation
+
+FFMPEG= 'ffmpeg'
+FFPROBE = 'ffprobe'
+
+def get_stream_resolution(stream_name):
+	metadata = {}
+	while 'streams' not in metadata:
+		info = subprocess.Popen([FFPROBE, '-v', '-quiet', '-print_format', 'json', '-show_format', '-show_streams', stream_name],
+			stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		out, err = info.communicate()
+		if out:
+			metadata = json.loads(out.decode('utf-8'))
+		time.sleep(5)
+
+	return metadata['streams'][0]['width'], metadata['streams'][0]['height']
+
+def get_frame_from_stream(resolution, pipe):
+	width, height = resolution
+	raw_image = pipe.stdout.read(width * height *3) # read 432*240*3 bytes (= 1 frame)
+	return numpy.fromstring(raw_image, dtype='uint8').reshape((width, height, 3))
+
+def main(argv):
+	stream_name = argv[1]
+	resolution = get_stream_resolution(stream_name)
+	stream = subprocess.Popen([FFMPEG, '-i', stream_name,
+		'-loglevel', 'quiet', # no text output
+		'-an',   # disable audio
+		'-f', 'image2pipe',
+		'-pix_fmt', 'bgr24',
+		'-vcodec', 'rawvideo', '-'],
+		stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	twitch_stream = subprocess.Popen([FFMPEG, '-i', '-',
+		'-vcodec', 'libx264', # no text output
+		'-an',   # disable audio
+		'-pix_fmt', 'yuv420p',
+		'-tune', 'fastdecode', 'rtmp://live-sjc.twtich.tv/apps/live_173288790_pEOfgLFUAfocVRZdAQ1D8bUubjL4OY'],
+		stdin = subprocess.PIPE, stdout = subprocess.PIPE)
+	while True:
+		frame = get_frame_from_stream(resolution, stream)
+		frame = pose_estimation.process_pose_frame(frame)
+		twitch_stream.communicate(input=frame.tobytes())
+
+if __name__ == '__main__':
+	main(sys.argv)
