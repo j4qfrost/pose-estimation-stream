111583a0bdf7b0d6cea96b40609beebc3fa8d7bc
Jack Frost <j4qfrost@gmail.com>
2019-09-18 00:27:31 -0700

    sync stuff

--- backend/pose_estimation.py
+++ backend/pose_estimation.py
@@ -1,53 +1,53 @@
 import time
 
 import cv2
-from gluoncv import model_zoo, data, utils
-from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
 
-detector = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True)
-pose_net = model_zoo.get_model('simple_pose_resnet18_v1b', pretrained=True)
+class PoseProcessor:
+	"""docstring for PoseProcessor"""
+	def __init__(self, framework):
+		super(PoseProcessor, self).__init__()
+		self.framework = framework
+		if framework == 'gluon':
+			
 
-# Note that we can reset the classes of the detector to only include
-# human, so that the NMS process is faster.
+			# Note that we can reset the classes of the detector to only include
+			# human, so that the NMS process is faster.
 
-detector.reset_class(["person"], reuse_weights=['person'])
 
-def process_pose_frame(np_frame, resolution):
-	width, height = resolution
-	if np_frame is None:
-	 	return mxnet.nd.zeros((height, width, 3))
-	
-	frame = mxnet.nd.array(np_frame)
-	x, img = data.transforms.presets.ssd.transform_test(frame, short=512)
+			self.process_pose_frame = build_pose_frame_function_gluon()
 
-	class_IDs, scores, bounding_boxs = detector(x)
+def build_pose_frame_function_gluon():
+	from gluoncv import model_zoo, data, utils
+	from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
+	import mxnet
+	detector = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True)
+	pose_net = model_zoo.get_model('simple_pose_resnet18_v1b', pretrained=True)
+	detector.reset_class(["person"], reuse_weights=['person'])
+	def process_pose_frame(np_frame, resolution):
+		width, height = resolution
+		if np_frame is None:
+		 	return mxnet.nd.zeros((height, width, 3))
+		
+		frame = mxnet.nd.array(np_frame)
+		x, img = data.transforms.presets.ssd.transform_test(frame, short=512)
 
-	pose_input, upscale_bbox = detector_to_simple_pose(img, class_IDs, scores, bounding_boxs)
+		class_IDs, scores, bounding_boxs = detector(x)
 
-	if pose_input is None:
-		return
+		pose_input, upscale_bbox = detector_to_simple_pose(img, class_IDs, scores, bounding_boxs)
 
-	predicted_heatmap = pose_net(pose_input)
-	pred_coords, confidence = heatmap_to_coord(predicted_heatmap, upscale_bbox)
+		if pose_input is None:
+			return
 
-	for j in range(len(pred_coords)):
-		for i in range(len(pred_coords[0])):
-			x, y = pred_coords[j][i].astype(int).asnumpy()
-			cv2.circle(img, (x,y), 2, (0, 255, 0), thickness=-1, lineType=cv2.FILLED)
-			# cv2.putText(img, tag, (x, y-20),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+		predicted_heatmap = pose_net(pose_input)
+		pred_coords, confidence = heatmap_to_coord(predicted_heatmap, upscale_bbox)
 
-	# for i in range(len(scores[0])):
-	#     #print(class_IDs.reshape(-1))
-	#     #print(scores.reshape(-1))
-	#     cid = int(class_IDs[0][i].asnumpy())
-	#     cname = detector.classes[cid]
-	#     score = float(scores[0][i].asnumpy())
-	#     if score < 0.5:
-	#         break
-	#     x,y,w,h = bbox =  bounding_boxs[0][i].astype(int).asnumpy()
-	#     print(cid, score, bbox)
-	#     tag = "{}; {:.4f}".format(cname, score)
-	#     cv2.rectangle(img, (x,y), (w, h), (0, 255, 0), 2)
-	#     cv2.putText(img, tag, (x, y-20),  cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0,0,255), 1)
+		for j in range(len(pred_coords)):
+			for i in range(len(pred_coords[0])):
+				x, y = pred_coords[j][i].astype(int).asnumpy()
+				cv2.circle(img, (x,y), 2, (0, 255, 0), thickness=-1, lineType=cv2.FILLED)
 
-	return img
+		return img
+	return process_pose_frame
+
+def process_pose_frame_torch(np_frame, resolution):
+	pass
---
+++ backend/pose_estimation_consumer_sync.py
@@ -0,0 +1,72 @@
+import subprocess, sys, time
+
+import json, numpy
+
+import cv2
+from twitchstream.outputvideo import TwitchBufferedOutputStream
+from pose_estimation import PoseProcessor
+
+FFMPEG= 'ffmpeg'
+FFPROBE = 'ffprobe'
+
+def get_stream_resolution(stream_name):
+	metadata = {}
+	while 'streams' not in metadata:
+		info = subprocess.run([FFPROBE, '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', '../hls/test.m3u8'],
+			capture_output=True)
+		out = info.stdout
+		if out:
+			metadata = json.loads(out.decode('utf-8'))
+		time.sleep(1)
+
+	return metadata['streams'][0]['width'], metadata['streams'][0]['height']
+
+def get_frame_from_stream(resolution, pipe):
+	width, height = resolution
+	raw_image = pipe.stdout.read(width * height *3) # read 432*240*3 bytes (= 1 frame)
+	if len(raw_image) == 0:
+		return None
+	return numpy.frombuffer(raw_image, dtype='uint8').reshape((height, width, 3))
+	
+def loop_send_frame(streamkey, resolution, stream, pose_processor):
+	videostream = TwitchBufferedOutputStream(
+            twitch_stream_key=streamkey,
+            width=resolution[0],
+            height=resolution[1],
+            fps=30.,
+            enable_audio=False,
+            verbose=False)
+	try:
+		while True:
+			frame = get_frame_from_stream(resolution, stream)
+			if frame is not None:
+				frame = pose_processor.process_pose_frame(frame, resolution)
+				videostream.send_video_frame(frame)
+	except Exception as e:
+		raise
+
+def save_image(img):
+	cv2.imwrite('test.jpg', img)
+
+def main(argv):
+	stream_name = argv[1]
+	pose_processor = PoseProcessor('gluon')
+	resolution = get_stream_resolution(stream_name)
+	stream = subprocess.Popen([FFMPEG, '-i', stream_name,
+		'-loglevel', 'quiet', # no text output
+		'-an',   # disable audio
+		'-f', 'image2pipe',
+		'-pix_fmt', 'bgr24',
+		'-vcodec', 'rawvideo', '-'],
+		stdout = subprocess.PIPE)
+	loop_send_frame('live_173288790_pEOfgLFUAfocVRZdAQ1D8bUubjL4OY', resolution, stream, pose_processor)
+
+	# while True:
+	# 	frame = get_frame_from_stream(resolution, stream)
+	# 	frame = pose_estimation.process_pose_frame(frame)
+
+	# 	if frame is not None:
+	# 		L.put(frame)
+	
+if __name__ == '__main__':
+	main(sys.argv)
