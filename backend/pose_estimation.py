f39bb5ba95fac028e5909d3eab96918b592cff02
Jack Frost <j4qfrost@gmail.com>
2019-09-24 01:43:04 -0700

    add dev env

--- backend/pose_estimation.py
+++ backend/pose_estimation.py
@@ -6,18 +6,55 @@ class PoseProcessor:
 		super(PoseProcessor, self).__init__()
 		self.framework = framework
 		if framework == 'tf':
-			self.process_pose_frame, self.gpu = build_pose_frame_function_tf()
+			self.process_pose_frame = build_pose_frame_function_tf()
 		if framework == 'gluon':
-			self.process_pose_frame, self.gpu = build_pose_frame_function_gluon()
+			self.process_pose_frame = build_pose_frame_function_gluon()
 
 def build_pose_frame_function_tf():
-	pass
+	import tensorflow as tf
+	import posenet
+	def process_pose_frame(np_frame, resolution):
+		frame = None
+		with tf.Session() as sess:
+			model_cfg, model_outputs = posenet.load_model(0, sess)
+			output_stride = model_cfg['output_stride']
+
+			input_image, draw_image, output_scale = posenet.process_input(
+					np_frame, output_stride=output_stride)
+
+			heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
+				model_outputs,
+				feed_dict={'image:0': input_image}
+			)
+
+			pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multiple_poses(
+				heatmaps_result.squeeze(axis=0),
+				offsets_result.squeeze(axis=0),
+				displacement_fwd_result.squeeze(axis=0),
+				displacement_bwd_result.squeeze(axis=0),
+				output_stride=output_stride,
+				max_pose_detections=1,
+				min_pose_score=0.25)
+
+			keypoint_coords *= output_scale
+
+			frame = posenet.draw_skel_and_kp(
+					draw_image, pose_scores, keypoint_scores, keypoint_coords,
+					min_pose_score=0.25, min_part_score=0.25)
+				
+
+		return frame
+
+
+	return process_pose_frame
 
 def build_pose_frame_function_gluon():
 	from gluoncv import model_zoo, data, utils
 	from gluoncv.data.transforms.pose import detector_to_simple_pose, heatmap_to_coord
 	from gluoncv.utils.viz import cv_plot_keypoints
 	import mxnet
+	import cv2
+
 	detector = model_zoo.get_model('yolo3_mobilenet1.0_coco', pretrained=True)
 	pose_net = model_zoo.get_model('simple_pose_resnet18_v1b', pretrained=True)
 
@@ -30,7 +67,7 @@ def build_pose_frame_function_gluon():
 		 	return mxnet.nd.zeros((height, width, 3), ctx=gpu)
 		
 		frame = mxnet.nd.array(np_frame, ctx=gpu)
-		x, img = data.transforms.presets.ssd.transform_test(frame, short=512)
+		x, img = data.transforms.presets.yolo.transform_test(frame, short=512)
 
 		class_IDs, scores, bounding_boxs = detector(x)
 
@@ -48,7 +85,7 @@ def build_pose_frame_function_gluon():
 		# 		x, y = pred_coords[j][i].astype(int).asnumpy()
 		# 		cv2.circle(img, (x,y), 2, (0, 255, 0), thickness=-1, lineType=cv2.FILLED)
 		return img
-	return process_pose_frame, gpu
+	return process_pose_frame
 
 def process_pose_frame_torch(np_frame, resolution):
 	pass
--- backend/pose_estimation_consumer_sync.py
+++ backend/pose_estimation_consumer_sync.py
@@ -1,4 +1,4 @@
-import subprocess, sys, time
+import subprocess, sys, time, os
 
 import json, numpy
 
@@ -6,53 +6,88 @@ import cv2
 from twitchstream.outputvideo import TwitchBufferedOutputStream
 from pose_estimation import PoseProcessor
 
+import tensorflow as tf
+import posenet
+
 FFMPEG= 'ffmpeg'
 FFPROBE = 'ffprobe'
 
 def get_stream_resolution(stream_name):
 	metadata = {}
 	while 'streams' not in metadata:
-		info = subprocess.run([FFPROBE, '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', '../hls/test.m3u8'],
-			capture_output=True)
+		info = subprocess.run([FFPROBE, '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', stream_name],
+			stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 		out = info.stdout
 		if out:
 			metadata = json.loads(out.decode('utf-8'))
 		time.sleep(1)
 
+	print('Grabbed resolution!')
 	return metadata['streams'][0]['width'], metadata['streams'][0]['height']
 
 def get_frame_from_stream(resolution, pipe):
 	width, height = resolution
-	raw_image = pipe.stdout.read(width * height *3) # read 432*240*3 bytes (= 1 frame)
+	raw_image = pipe.stdout.read(width * height * 3) # read 432*240*3 bytes (= 1 frame)
 	if len(raw_image) == 0:
 		return None
 	return numpy.frombuffer(raw_image, dtype='uint8').reshape((height, width, 3))
 	
 def loop_send_frame(streamkey, resolution, stream, pose_processor):
-	videostream = TwitchBufferedOutputStream(
-            twitch_stream_key=streamkey,
-            width=resolution[0],
-            height=resolution[1],
-            fps=30.,
-            enable_audio=False,
-            verbose=False)
+	width, height = resolution
 	try:
-		while True:
-			frame = get_frame_from_stream(resolution, stream)
-			if frame is not None:
-				print(frame.size)
-				frame = pose_processor.process_pose_frame(frame, resolution)
-				print(frame.size)
-				videostream.send_video_frame(frame)
+		config = tf.ConfigProto()
+		config.intra_op_parallelism_threads = 4
+		config.inter_op_parallelism_threads = 4
+		with TwitchBufferedOutputStream(
+            twitch_stream_key=streamkey,
+            width=width,
+            height=height,
+            fps=15.,
+            enable_audio=False,
+            verbose=False) as videostream:
+			with tf.Session(config=config) as sess:
+				model_cfg, model_outputs = posenet.load_model(3, sess)
+
+				frame = tf.placeholder(tf.uint8, shape=(height, width, 3))
+				input_image = tf.placeholder(tf.uint8, shape=(1, height + 1, width + 1, 3))
+
+				while True:
+					frame = get_frame_from_stream(resolution, stream)
+					if frame is not None:
+						start = time.time()
+						output_stride = model_cfg['output_stride']
+
+						input_image, frame, output_scale = posenet.process_input(
+								frame, output_stride=output_stride)
+						heatmaps_result, offsets_result, displacement_fwd_result, displacement_bwd_result = sess.run(
+							model_outputs,
+							feed_dict={'image:0': input_image}
+						)
+						pose_scores, keypoint_scores, keypoint_coords = posenet.decode_multiple_poses(
+							heatmaps_result.squeeze(axis=0),
+							offsets_result.squeeze(axis=0),
+							displacement_fwd_result.squeeze(axis=0),
+							displacement_bwd_result.squeeze(axis=0),
+							output_stride=output_stride,
+							max_pose_detections=1, min_pose_score=0.10)
+
+						keypoint_coords *= output_scale
+
+						frame = posenet.draw_skel_and_kp(
+								frame, pose_scores, keypoint_scores, keypoint_coords,
+								min_pose_score=0.10, min_part_score=0.10)
+						videostream.send_video_frame(frame)
+						print(time.time() - start)
 	except Exception as e:
 		raise
 
 def save_image(img):
 	cv2.imwrite('test.jpg', img)
 
-def main(argv):
-	stream_name = argv[1]
-	pose_processor = PoseProcessor('gluon')
+def main(stream_name):
+	print('Starting program...')
+	# stream_name = argv[1]
+	pose_processor = PoseProcessor('tf')
 	resolution = get_stream_resolution(stream_name)
 	stream = subprocess.Popen([FFMPEG, '-i', stream_name,
 		'-loglevel', 'quiet', # no text output
@@ -60,7 +95,7 @@ def main(argv):
 		'-f', 'image2pipe',
 		'-pix_fmt', 'bgr24',
 		'-vcodec', 'rawvideo', '-'],
-		stdout = subprocess.PIPE)
+		stdout = subprocess.PIPE, stderr=subprocess.PIPE)
 	loop_send_frame('live_173288790_pEOfgLFUAfocVRZdAQ1D8bUubjL4OY', resolution, stream, pose_processor)
 
 	# while True:
@@ -71,4 +106,4 @@ def main(argv):
 	# 		L.put(frame)
 	
 if __name__ == '__main__':
-	main(sys.argv)
+	main(sys.argv[1])
